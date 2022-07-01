154130a0233381beedc274fb25978847f0fbe295
Dos <doszlmnv@gmail.com>
2022-07-01 16:56:30 +0600

    TODO: Project38  - 1

--- Places/CoreData/PointOnMap+CoreDataProperties.swift
+++ Places/CoreData/PointOnMap+CoreDataProperties.swift
@@ -12,22 +12,22 @@ import CoreData
 
 extension PointOnMap {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<PointOnMap> {
+    @nonobjc public class func createFetchRequest() -> NSFetchRequest<PointOnMap> {
         return NSFetchRequest<PointOnMap>(entityName: "PointOnMap")
     }
 
-    @NSManaged public var title: String?
-    @NSManaged public var details: String?
+    @NSManaged public var title: String
+    @NSManaged public var details: String
     @NSManaged public var longitude: Double
     @NSManaged public var latitude: Double
     
-    public var wrappedTitle: String {
-        title ?? "Unkown title"
-    }
-    
-    public var wrappedDetails: String {
-        details ?? "Unknown details"
-    }
+//    public var wrappedTitle: String {
+//        title ?? "Unkown title"
+//    }
+//
+//    public var wrappedDetails: String {
+//        details ?? "Unknown details"
+//    }
 }
 
 extension PointOnMap : Identifiable {
